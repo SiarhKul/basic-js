53de2790c75b8d902a6507cb85da0613d48a9664
SiarhKul <SiarhKul@gmail.com>
2021-01-24 16:38:14 +0300

    fix: simple-chain

--- src/simple-chain.js
+++ src/simple-chain.js
@@ -1,35 +1,19 @@
 const chainMaker = {
   arr: [],
 
-  getLength() {
-    return this.arr.length;
-  },
+  getLength() { return this.arr.length; },
 
-  addLink(value) {
-    this.arr.push(`( ${value} )`);
-    return this;
-  },
+  addLink(value) { this.arr.push(`( ${value} )`); return this; },
 
   removeLink(position) {
-    if (position > this.arr.length || position <= 0 || isNaN(position)) {
-      this.arr = [];
-      throw new Error;
-    } else {
-      this.arr.splice(position - 1, 1);
-      return this;
-    }
-  },
-
-  reverseChain() {
-    this.arr.reverse();
+    if (position > this.arr.length || position <= 0 || isNaN(position)) { this.arr = []; throw new Error; }
+    this.arr.splice(position - 1, 1);
     return this;
-  },
 
-  finishChain() {
-    let result = this.arr.join('~~');
-    this.arr = [];
-    return result;
-  }
+  },
+  reverseChain() { this.arr.reverse(); return this; },
+
+  finishChain() { let result = this.arr.join('~~'); this.arr = []; return result; }
 };
 
 module.exports = chainMaker;
